b81702a96c4403e478f9f0ad40ced3156b999eeb
Paul Allen <p.allen.art@gmail.com>
2018-02-25 22:01:23 -0700

    Nav animations

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,52 @@
+const anime = require('animejs');
+
 const navTrigger = document.querySelector('.js-toggle-nav');
 const overlay = document.querySelector('.js-nav-overlay');
+const nav = document.getElementById('js-nav');
+const navItems = nav.querySelectorAll('li');
 const body = document.querySelector('body');
 
-navTrigger.addEventListener('click', e => body.classList.toggle('nav-is-open'));
-overlay.addEventListener('click', e => body.classList.toggle('nav-is-open'));
+//hide items on load
+window.addEventListener('load', () => {
+	hide();
+});
+
+// open
+navTrigger.addEventListener('click', e => {
+	body.classList.toggle('nav-is-open');
+
+	// animate in nav items
+	if (body.classList.contains('nav-is-open')) {
+		const showItems = anime({
+			targets: navItems,
+			translateX: 0,
+			opacity: {
+				value: 1,
+				elasticity: 0,
+			},
+			elasticity: 100,
+			delay: function (el, i, l) {
+				return i * 100;
+			}
+		});
+	} else {
+		hide();
+	}
+});
+
+// close
+overlay.addEventListener('click', e => {
+	body.classList.toggle('nav-is-open') }
+);
+
+/**
+* functions
+*/
+function hide() {
+	const hideItems = anime({
+		targets: navItems,
+		translateX: '25%',
+		opacity: 0,
+		delay: 500,
+	});
+}
